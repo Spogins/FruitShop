69c70dc8ee93382d3576c1561671b04748964f0d
spogins <spogins@gmail.com>
2023-07-17 15:13:08 +0300

    docker

--- fruit_shop/settings.py
+++ fruit_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
